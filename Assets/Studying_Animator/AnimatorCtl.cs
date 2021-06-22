c6b9c4825d479b0bcccc100cc1a0eda040d62806
Audin <audin@diamondonline.com.tw>
2021-06-22 14:32:20 +0800

    studying animator

---
+++ Assets/Studying_Animator/AnimatorCtl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorCtl : MonoBehaviour
+{
+    Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+
+    /// <summary>
+    /// 給 Animation 的 Event 使用
+    /// </summary>
+    public void Test()
+    {
+        Debug.Log("test");
+    }
+
+    public void SetTrigger_Scale_Roll() => _animator.SetTrigger("Scale_Roll");
+
+    public void SetBool_IsScale(bool b) => _animator.SetBool("IsScale", b);
+
+    public void SetFloat_Scale_Speed(float rate = 1) => _animator.SetFloat("Scale_Speed", rate);
+}
