7fd8fd17d467b0c30ed1fbdd46f69f8edfc51de4
Audin <audin@diamondonline.com.tw>
2021-09-09 14:43:24 +0800

    add multi-switch by toggle group

---
+++ Assets/Studying_Toggle/ToggleCtl.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(ToggleGroup))]
+public class ToggleCtl : MonoBehaviour
+{
+    ToggleGroup _toggleGroup;
+
+    private void Start()
+    {
+        _toggleGroup = GetComponent<ToggleGroup>();
+    }
+
+    public void Who(int id)
+    {
+        Debug.Log(id);
+    }
+}
